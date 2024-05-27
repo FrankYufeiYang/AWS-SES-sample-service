b2d9edca4ca86263e8a85e22ff5b697c657c0bea
YUFEI YANG <YUFEIYANG.frank@foxmail.com>
2024-05-27 17:44:47 -0400

    set up node environment

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import cors from "cors"
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/', express.static('public'))
+const port = 3002;
+app.get('/hello',(req, response) =>{
+  response.status(200).send('hello');
+
+})
+
+app.listen(port, ()=>{
+  console.log("running on "+port);
+})
